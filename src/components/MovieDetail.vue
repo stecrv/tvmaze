500aa248896d614a9ffe1d957bdddd6f1bca7f2b
stefano caravana <stecrv@users.noreply.github.com>
2023-09-26 18:15:27 +0000

    detail page

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="movie-details__container container">
-    <div class="row">
-      <div class="eight columns">
-    <h2>Movie Details</h2>
+  <div class="movie-details__container container">    
+    <h1>Movie details</h1>
     <div v-if="fetch.load" class="loading-message">
       Loading data..
     </div>
@@ -11,25 +9,37 @@
     </div>
     <div v-else>
       <div class="movie-details__content">
-        <h3>{{ fetch.data.name }}</h3>
-        <img :src="fetch.data.image.medium" :alt="fetch.data.name" />
-        <p v-html="fetch.data.summary" />
+        <h3>{{ movie.name }}</h3>
+        <div class="movie-details__figure">
+          <div class="movie-details__column">
+            <img :src="movie.image.medium" :alt="movie.name" />
+          </div>
+          <div class="movie-details__text">
+            <div class="movie-details__info">Year: {{ movie.year }}</div>
+            <div class="movie-details__info">Rating: {{ movie.rating.average ?? 'unknow' }}</div>
+            <div class="movie-details__info">
+              <a :href="movie.url" target="_blank" class="button">
+                Website
+              </a>
+            </div>
+          </div>
+        </div>
+        <p v-html="movie.summary" />
       </div>
     </div>
     <div>
-      <a @click="$router.go(-1)" class="button">
+      <a @click="$router.go(-1)" class="button button-primary">
         Back
       </a>
-    </div>
-    </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import {computed, reactive} from 'vue';
+import { computed, reactive } from 'vue';
 import { useRoute } from 'vue-router';
 import { useFetch } from "../composables/useFetch.js";
+import { dataMovieEnricher } from '../helpers/functions' 
 
 
 const route = useRoute();
@@ -38,12 +48,36 @@ const movieId = computed(() => route.params.id);
 
 const fetch = reactive(useFetch(`shows/${movieId.value}`));
 
+const movie = computed(() => {
+  return fetch.data ? dataMovieEnricher(fetch.data) : {}
+});
+
 </script>
 
 <style scoped>
 .movie-details__container{
-  padding: 10px;
-  margin: 10px;
-  margin: 0 auto;
+  margin: 10vh auto 0 auto;
+  max-width: 400px;
 }
+.movie-details__figure{
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+}
+.movie-details__info{
+  margin-bottom: 1rem;
+}
+
+
+@media (max-width: 550px) {
+  .movie-details__container{
+    margin-top: 5vh;
+    max-width: 90vw;
+  }
+  .movie-details__figure{
+    flex-direction: column; 
+    gap: 10px;
+  }
+}
+
 </style>
--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,5 +1,9 @@
  export const dataEnricher = data => {
-    //return data
     return data.map(el => {
         return {...el, abstract: el.summary.substring(0, 200) + "..."}});
+ }
+
+ export const dataMovieEnricher = movie =>{
+        const year = movie?.premiered?.split('-') ?? ['none'];
+        return {...movie, year: year[0]}
  }
